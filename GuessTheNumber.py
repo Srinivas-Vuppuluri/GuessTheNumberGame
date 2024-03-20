d4260d2c4f683d775c0d9a6e1185c1d7effbbe97
Srinivas-Vuppuluri <159790218+Srinivas-Vuppuluri@users.noreply.github.com>
2024-03-20 12:47:15 +0530

    Update GuessTheNumber.py

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,52 +1,48 @@
-import random #bring in the random number
-import time
-number=random.randint(1, 200) #pick the number between 1 and 200
+import random
 
-def intro():
+def number_guessing_game():
+    lower_bound = 1
+    upper_bound = 100
+    secret_number = random.randint(lower_bound, upper_bound)
+    max_attempts = 10
+    attempts = 0
+    
     print("May I ask you for your name?")
     name=input() #asks for the name
-    print(name + ", we are going to play a game. I am thinking of a number between 1 and 200")
-    time.sleep(.5)
-    print("Go ahead. Guess!")
+    print(name + ", Welcome to the Number Guessing Game!")
+    print(f"Guess a number between {lower_bound} and {upper_bound}.")
 
-def pick():
-    guessesTaken = 0
-    while guessesTaken < 6: #if the number of guesses is less than 6
-        time.sleep(.25)
-        enter=input("Guess: ") #inserts the place to enter guess
-        try: #check if a number was entered
-            guess = int(enter) #stores the guess as an integer instead of a string    
+    while attempts < max_attempts:
+        user_guess = get_user_input(lower_bound, upper_bound)
+        attempts += 1
 
-            if guess<=200 and guess>=1: #if they are in range
-                guessesTaken=guessesTaken+1 #adds one guess each time the player is wrong
-                if guessesTaken<6:
-                    if guess<number:
-                        print("The guess of the number that you have entered is too low")
-                    if guess>number:
-                        print("The guess of the number that you have entered is too high")
-                    if guess != number:
-                        time.sleep(.5)
-                        print("Try Again!")
-                if guess==number:
-                    break #if the guess is right, then we are going to jump out of the while block
-            if guess>200 or guess<1: #if they aren't in the range
-                print("Silly Goose! That number isn't in the range!")
-                time.sleep(.25)
-                print("Please enter a number between 1 and 200")
+        if user_guess == secret_number:
+            print(f"Congratulations! You guessed the secret number {secret_number} in {attempts} attempts.")
+            break
+        elif user_guess < secret_number:
+            print("Try again! Your guess is too low.")
+        else:
+            print("Try again! Your guess is too high.")
 
-        except: #if a number wasn't entered
-            print("I don't think that "+enter+" is a number. Sorry")
-            
-    if guess == number:
-        guessesTaken = str(guessesTaken)
-        print('Good job, ' + name + '! You guessed my number in ' + guessesTaken + ' guesses!')
+    if attempts == max_attempts:
+        print(f"Sorry, you've reached the maximum number of attempts. The secret number was {secret_number}.")
 
-    if guess != number:
-        print('Nope. The number I was thinking of was ' + str(number))
+def get_user_input(lower_bound, upper_bound):
+    while True:
+        try:
+            user_guess = int(input(f"Enter your guess ({lower_bound}-{upper_bound}): "))
+            if lower_bound <= user_guess <= upper_bound:
+                return user_guess
+            else:
+                print(f"Please enter a number between {lower_bound} and {upper_bound}.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
+if __name__ == "__main__":
+    number_guessing_game()
+
+print("Do you want to play again?")
+playagain=input()
 playagain="yes"
 while playagain=="yes" or playagain=="y" or playagain=="Yes":
-    intro()
-    pick()
-    print("Do you want to play again?")
-    playagain=input()
+    number_guessing_game()
